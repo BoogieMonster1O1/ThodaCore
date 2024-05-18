3d28524c9689894a41b66ecc0a2ddcf76dd2e396
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-05-18 16:33:42 +0530

    commit

--- Sources/App/Models/LostFoundItem.swift
+++ Sources/App/Models/LostFoundItem.swift
@@ -13,4 +13,38 @@ final class LostFoundItem: Model, Content {
 
     @ID(custom: "id", generatedBy: .user)
     var id: String?
+
+    @Parent(key: "user_id")
+    var creator: RegisteredUser
+
+    @Field(key: "title")
+    var title: String
+
+    @Field(key: "content")
+    var content: String
+
+    @Field(key: "isFoundItem")
+    var isFoundItem: Bool
+
+    @Timestamp(key: "created_at", on: .create)
+    var createdAt: Date?
+
+    @Timestamp(key: "deleted_at", on: .delete)
+    var deletedAt: Date?
+
+    @Timestamp(key: "closed_at", on: .none)
+    var closedAt: Date?
+
+    @Enum(key: "itemType")
+    var itemType: LostItemType
+
+    @Field(key: "imageUrl")
+    var imageUrl: String?
+}
+
+enum LostItemType: String, Content {
+    case idCard
+    case earphones
+    case calculator
+    case other
 }
